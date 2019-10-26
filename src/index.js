7f57af4def517c2ab6bfc1e4f41ae12559bf759c
Maciej Krüger <mkg20001@gmail.com>
2019-10-27 01:15:56 +0200

    fix: stuff

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ module.exports = async (config, arweave) => {
   const cache = await Cache(config.cache)
   const swarm = await Swarm(config.swarm, cache)
 
-  swarm._node.peerInfo.multiaddrs.toArray().map(String).forEach(addr => {
+  swarm.node.peer().addrs.forEach(addr => {
     console.log('Listening on %s', addr)
   })
 }
